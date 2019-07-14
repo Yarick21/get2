a4d56d400fc381ea06102f73a674ce40ee3a34c1
Yachmenev <silvansx2@yandex.com>
2019-07-14 22:58:29 +0300

    information output by id

---
+++ get2.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request, abort
+
+
+app = Flask(__name__)
+
+Yachmenev = [
+    {
+        'id': 1,
+        'subject': u'vichmat',
+        'rating': u'4'
+    },
+    {
+        'id': 2,
+        'subject': u'Bondarb',
+        'rating': u'4'
+    },
+    {
+        'id': 3,
+        'subject': u'OS',
+        'rating': u'5'
+    },
+    {
+        'id': 4,
+        'subject': u'EVM',
+        'rating': u'5'
+    }
+]
+
+@app.route('/api/Yachmenev', methods=['GET'])
+def get_Yachmenev():
+    return jsonify({'Yachmenev': Yachmenev})
+
+@app.route('/api/Yachmenev/<int:nomer>', methods=['GET'])
+def get2_Yachmenev(nomer):
+    return jsonify({'Yachmenev': Yachmenev[nomer-1]})
